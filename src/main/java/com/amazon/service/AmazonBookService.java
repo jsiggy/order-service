82c22da8a557fd46dfb14e4f01b478c8133d6c6c
john.sigler <john.sigler@gmail.com>
2017-08-28 21:33:41 -0700

    Cleanup book service

--- src/main/java/com/amazon/service/AmazonBookService.java
+++ src/main/java/com/amazon/service/AmazonBookService.java
@@ -1,23 +1,32 @@
 package com.amazon.service;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AmazonBookService {
+
+    List<String> productIdCatalog = Arrays.asList("Foo", "Baz", "Zot");
 
    public boolean placeOrder(String productId, int units) throws InvalidAmazonProductIdException, InvalidAmazonProductAmountException {
       System.out.println("calling Amazon Book Service for " + units + " units of " + productId);
-      simulateNetworkLatencyDelay();
-      if (productId == null || productId.length() < 5)
+      simulateLongNetworkLatencyDelay();
+      if (productId == null || ! productIdCatalog.contains(productId))
          throw new InvalidAmazonProductIdException("Invalid product id: " + productId);
       if (units < 0)
          throw new InvalidAmazonProductAmountException("Product amount cannot be negative - " + units);
 
-      return units < 10;
+      return hasSufficientInventory(productId, units);
    }
 
-   private static void simulateNetworkLatencyDelay() {
+    private static void simulateLongNetworkLatencyDelay() {
       try {
          Thread.sleep(10000);
       } catch (InterruptedException e) {
          e.printStackTrace();
       }
    }
+
+    private boolean hasSufficientInventory(String productId, int units) {
+        return units < 10;
+    }
 }
